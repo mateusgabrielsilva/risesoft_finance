fb1bd4d2be93e95ef2e5a2b2d0ad3ae9cf78721d
Mateus Gabriel <mateus92.mgs@gmail.com>
2024-11-08 08:07:00 -0300

    feat: init login page

---
+++ app/login/page.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import { Button } from "../_components/ui/button";
+import { LogInIcon } from "lucide-react";
+
+const LoginPage = () => {
+  return (
+    <div className="grid h-full grid-cols-2">
+      {/* Esquerda */}
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/logo.svg"
+          width={173}
+          height={39}
+          alt="Logo Risesoft Finance"
+          className="mb-8"
+        />
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
+        <p className="mb-8 text-muted-foreground">
+          O Risesoft Finance é uma plataforma de gestão financeira que utiliza
+          IA para monitorar suas movimentações, e oferecer insights
+          personalizados, facilitando o controle do seu orçamento.
+        </p>
+        <Button variant="outline">
+          Fazer login ou criar conta <LogInIcon className="mr-2" />
+        </Button>
+      </div>
+      {/* Direita */}
+      <div className="relative h-full w-full">
+        <Image
+          src="/login.png"
+          alt="Faça login"
+          fill
+          className="object-cover"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
