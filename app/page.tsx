9ebc812bbcd7883ec1916fb36dcaa34848843d12
Mateus Gabriel <mateus92.mgs@gmail.com>
2024-11-08 07:40:51 -0300

    chore: shadcn add setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./_components/ui/button";
+
 const HomePage = () => {
   return (
     <div className="flex w-screen items-center justify-center">
       <h1 className="p-5 text-orange-500">hello world</h1>
+      <Button>Olá</Button>
     </div>
   );
 };
